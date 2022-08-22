68a134a30d2f0321856fe777c4e99091abdb1c97
Andra-Stefania Belu <andra.belu@student.upt.ro>
2022-08-22 12:26:25 +0300

    url api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
-import './App.css';
+import React, {useState} from 'react'
+import axios from 'axios'
 
 function App() {
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=43.985489&lon=22.9446255&appid=ea5ec0ba2f81de8d7f3d76bd1a95e0f8`
+
   return (
     <div className="App">
       
