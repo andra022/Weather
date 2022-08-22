06d6ce3a60dc40dbd3e741562323276b4853d53c
Andra-Stefania Belu <andra.belu@student.upt.ro>
2022-08-22 15:06:39 +0300

    design

--- src/App.js
+++ src/App.js
@@ -45,11 +45,11 @@ function App() {
           <div className="bottom">
             <div className="feels">
               {data.main ? <p className="bold">{data.main.feels_like.toFixed()}°C</p> : null}
-              <p>Feels Like</p>
+              <p>Feels Like </p>
             </div>
             <div className="humidity">
               {data.main ? <p className="bold">{data.main.humidity}%</p> : null}
-              <p>Humidity</p>
+              <p>Humidity </p>
             </div>
             <div className="wind">
               {data.wind ? <p className="bold">{data.wind.speed}MPH</p> : null}
